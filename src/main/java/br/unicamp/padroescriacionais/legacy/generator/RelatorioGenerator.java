b616baf66fa88aafd24c618a87e24725f5093e47
Augusto Toledo Caires de Oliveira <augustotoledo23@gmail.com>
2026-05-17 16:04:52 -0300

    Criando interfaces para a estrutura Factory Method
    Basicamente essas interfaces criarão a assinatura em comum para as factories que vão criar os geradores de relatório, estes que por sua vez cumprirão com a assinatura da interface RelatorioGenerator, assim apenas alterando a forma que o método gerar() se comporta.

---
+++ src/main/java/br/unicamp/padroescriacionais/legacy/generator/RelatorioGenerator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.unicamp.padroescriacionais.legacy.generator;
+
+import br.unicamp.padroescriacionais.legacy.domain.Relatorio;
+
+/**
+ *
+ * @author User
+ */
+public interface RelatorioGenerator {
+    String gerar(Relatorio relatorio); 
+}
---
+++ src/main/java/br/unicamp/padroescriacionais/legacy/generator/RelatorioGeneratorFactory.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.unicamp.padroescriacionais.legacy.generator;
+
+/**
+ *
+ * @author User
+ */
+public interface RelatorioGeneratorFactory {
+    
+    RelatorioGenerator createGenerator(); 
+}
