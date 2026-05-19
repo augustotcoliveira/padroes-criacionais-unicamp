7b6ee5f36f185a930ea97124f9dfb1a713cd74bd
Augusto Toledo Caires de Oliveira <66071603+augustotcoliveira@users.noreply.github.com>
2026-05-19 15:20:10 +0000

    Removendo comentarios desnecessarios nos novos arquivos gerados

--- src/main/java/br/unicamp/padroescriacionais/legacy/generator/PdfRelatorioGeneratorFactory.java
+++ src/main/java/br/unicamp/padroescriacionais/legacy/generator/PdfRelatorioGeneratorFactory.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.unicamp.padroescriacionais.legacy.generator;
 
-/**
- *
- * @author User
- */
 public class PdfRelatorioGeneratorFactory implements RelatorioGeneratorFactory{
     
     @Override
--- src/main/java/br/unicamp/padroescriacionais/legacy/generator/RelatorioGenerator.java
+++ src/main/java/br/unicamp/padroescriacionais/legacy/generator/RelatorioGenerator.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package br.unicamp.padroescriacionais.legacy.generator;
 
 import br.unicamp.padroescriacionais.legacy.domain.Relatorio;
 
-/**
- *
- * @author User
- */
 public interface RelatorioGenerator {
     String gerar(Relatorio relatorio); 
 }
